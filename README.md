d5abefd05d22eb7a1f7802232f2303ece4d84d65
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-04-20 11:21:18 +0200

    Added README and LICENSE, updated to take command line argument for the folder

---
+++ README.md
@@ -0,0 +1,19 @@
+# Google Drive Downloader
+
+Downloads all files in the specified Google Drive folder id.
+
+## Installing
+
+```bash
+git clone git@github.com:ashleykleynhans/google-drive-downloader.git
+cd google-drive-downloader
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+## Running
+
+```bash
+python3 download_folder --folder-id <FOLDER_ID>
+```
--- download_folder.py
+++ download_folder.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import io
 import pickle
@@ -9,55 +10,54 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaIoBaseDownload
 
-# Replace with your folder ID
-folder_id = '1YvFj33Bfum4YuBeqNNCYLfiBrD4tpzg7'
 
-# Initialize the Drive API client
-creds = None
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Download all files from a Google Drive folder.',
+    )
 
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
+    parser.add_argument(
+        '--folder-id', '-folder-id', '--f', '-f',
+        type=str,
+        required=True,
+        help='AWS region (eg. us-east-1)'
+    )
 
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', ['https://www.googleapis.com/auth/drive'])
-        creds = flow.run_local_server(port=0)
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
+    return parser.parse_args()
 
-service = build('drive', 'v3', credentials=creds)
 
-# Define a function to download a file from Drive
 def download_file(file_id, file_name):
     request = service.files().get_media(fileId=file_id)
     file = io.BytesIO()
     downloader = MediaIoBaseDownload(file, request)
     done = False
+
     while done is False:
         status, done = downloader.next_chunk()
         print(f'Download {int(status.progress() * 100)}.')
+
     file.seek(0)
+
     with open(file_name, 'wb') as f:
         f.write(file.read())
 
-# Define a function to get all files in a folder
+
 def get_files_in_folder(folder_id):
     results = []
     query = f"'{folder_id}' in parents and trashed = false"
     page_token = None
+
     while True:
         try:
             response = service.files().list(q=query,
                                             spaces='drive',
                                             fields='nextPageToken, files(id, name)',
                                             pageToken=page_token).execute()
+
             files = response.get('files', [])
             results.extend(files)
             page_token = response.get('nextPageToken', None)
+
             if page_token is None:
                 break
         except HttpError as error:
@@ -65,13 +65,36 @@ def get_files_in_folder(folder_id):
             break
     return results
 
-# Get all files in the folder
-files = get_files_in_folder(folder_id)
 
-# Download each file
-for file in files:
-    file_id = file['id']
-    file_name = file['name']
-    print(f'Downloading {file_name}...')
-    download_file(file_id, file_name)
+if __name__ == '__main__':
+    args = get_args()
+    folder_id = args.folder_id
 
+    # Initialize the Drive API client
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', ['https://www.googleapis.com/auth/drive'])
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Get all files in the folder
+    files = get_files_in_folder(folder_id)
+
+    # Download each file
+    for file in files:
+        file_id = file['id']
+        file_name = file['name']
+        print(f'Downloading {file_name}...')
+        download_file(file_id, file_name)
